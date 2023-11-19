046adf9f2f301afc216f3d2f2afa967843968a91
Ross Williams <gunzy83au@gmail.com>
2023-11-19 19:50:48 +1000

    New version of gunzy-os bringing in changes from startingpoint 1.x

---
+++ config/scripts/install-wavebox.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+echo 'Installing wavebox (installs to /opt) and handle symlinking...'
+
+mkdir /var/opt
+
+echo 'Installing wavebox browser...'
+rpm -Uvh https://download.wavebox.app/latest/stable/linux/rpm
+mv /var/opt/wavebox.io/wavebox/ /usr/lib/wavebox
+echo 'L /opt/wavebox.io/wavebox - - - - ../../../usr/lib/wavebox' > /usr/lib/tmpfiles.d/wavebox.conf
---
+++ config/scripts/setup-pam-u2f.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+echo 'Setting up PAM for U2F keys...'
+sed '/^auth       include      system-auth/i auth    sufficient    pam_u2f.so cue origin=pam://hostname appid=pam://hostname' /etc/pam.d/polkit-1 | sudo tee /usr/etc/pam.d/polkit-1
